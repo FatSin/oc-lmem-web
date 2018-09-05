8431215ad712a56619975b710e6f549b0369b448
FatSin <sonicfbis@hotmail.com>
2018-09-05 14:07:28 +0200

    Test heroku

--- lememeenmieux/wsgi.py
+++ lememeenmieux/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.settings")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux/lememeenmieux.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.lememeenmieux.settings")
 
 
 application = get_wsgi_application()
