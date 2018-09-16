c854ccd9f4a8c05e62345cd56e0b47a5d06f5241
FatSin <sonicfbis@hotmail.com>
2018-09-16 21:43:04 +0200

    Still trying to make it work on Heroku...

--- lememeenmieux/lememeenmieux/wsgi.py
+++ lememeenmieux/lememeenmieux/wsgi.py
@@ -11,11 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-if os.environ.get('ENV') == 'PRODUCTION':
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.lememeenmieux.settings")
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.settings")
+#if os.environ.get('ENV') == 'PRODUCTION':
+#    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.lememeenmieux.settings")
+#else:
+#    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.settings")
 
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.lememeenmieux.settings")
 
 application = get_wsgi_application()
--- lememeenmieux/manage.py
+++ lememeenmieux/manage.py
@@ -3,14 +3,14 @@ import os
 import sys
 
 if __name__ == "__main__":
-    
-    if os.environ.get('ENV') == 'PRODUCTION':
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.lememeenmieux.settings")
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.settings")
+
+    #if os.environ.get('ENV') == 'PRODUCTION':
+    #    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.lememeenmieux.settings")
+    #else:
+    #    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.settings")
 
     #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.settings")
-    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.lememeenmieux.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.lememeenmieux.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
