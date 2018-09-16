559b695770a177739228cd93f2323e1a171a3e40
FatSin <sonicfbis@hotmail.com>
2018-09-16 23:06:03 +0200

    Still trying to make it work on Heroku...

--- lememeenmieux/lememeenmieux/settings.py
+++ lememeenmieux/lememeenmieux/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -120,6 +122,15 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+
+
+
+
+if os.environ.get('ENV') == 'PRODUCTION':
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
