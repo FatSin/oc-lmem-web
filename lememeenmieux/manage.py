808f7c072fadfbaef24bc9a817185a42a328d0be
FatSin <sonicfbis@hotmail.com>
2018-09-16 21:27:16 +0200

    Still trying to make it work on Heroku...

--- lememeenmieux/manage.py
+++ lememeenmieux/manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.settings")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.lememeenmieux.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lememeenmieux.lememeenmieux.toudoudou")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
